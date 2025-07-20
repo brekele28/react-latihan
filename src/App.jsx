03d7bcd75af42edd0bc27a4998bedcd30da327a1
2025NANDA <roisalifernanda28@gmail.com>
2025-07-20 20:04:06 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      {/* Hero Image */}
+      <h1 className='text-4xl text-red-500 font-bold'>Hello World!!!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <i class="ri-mail-fill"></i>
+    </div>
+  );
+};
+
+export default Navbar;
