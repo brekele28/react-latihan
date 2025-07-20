685180996028f9686f0b0c6e2b412c2ea674f642
2025NANDA <roisalifernanda28@gmail.com>
2025-07-21 00:32:27 +0700

    Update tampilan halaman utama di App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
       {/* Hero Image */}
       <h1 className='text-4xl text-red-500 font-bold'>Hello World!!!</h1>
       <h2>Perkenalkan Nama Saya: Rois Ali Fernanda</h2>
+      <h4>Mahasiswa semester 6</h4>
     </>
   )
 }
