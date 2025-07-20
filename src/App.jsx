ff921699f94ef154c4db0bb9e171fb19f70864f0
2025NANDA <roisalifernanda28@gmail.com>
2025-07-21 00:37:36 +0700

    Coba fitur baru

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1 className='text-4xl text-red-500 font-bold'>Hello World!!!</h1>
       <h2>Perkenalkan Nama Saya: Rois Ali Fernanda</h2>
       <h4>Mahasiswa semester 6</h4>
+      <h2>cobain ubah dulu</h2>
     </>
   )
 }
